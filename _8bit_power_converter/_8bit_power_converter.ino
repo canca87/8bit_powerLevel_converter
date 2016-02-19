40cfd2f77ed51cda548399c5cb36a03e8416f0f6
Adam Chrimes <chrimesa@ICB-DEM-01.d.ethz.ch>
2016-02-19 15:36:29 +0100

    Project Started
    Empty code structure

---
+++ _8bit_power_converter/_8bit_power_converter.ino
@@ -0,0 +1,14 @@
+//-------Includes ---------------
+//-------Defines ---------------
+//-------Variables ---------------
+//-------Objects ---------------
+//-------Setup ---------------
+void setup() {
+  // put your setup code here, to run once:
+
+}
+//-------Main ---------------
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
